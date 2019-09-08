81f652a10487222575555576c05827cb1a4e7696
Dana Simmons <dsanalog@gmail.com>
2019-09-08 08:20:12 -0400

    Add Express JS

--- app.js
+++ app.js
@@ -5,36 +5,18 @@
 */
 
 const http = require('http');
-const userform = require('./userform');
+const express = require('express');
 
+const app = express();
 
+app.use( ( req, res, next ) =>{
+  console.log("Stuck in the middle");
+  next();
+})
 
-const handler = ( req, res ) =>{
-  if( req.url === '/' ){
-    res.write( userform );
-    return res.end();
-  }
-
-  if ( req.url === '/users' ){
-    res.write("<html><ol><li>Bob</li><li>Jill</li><li>Jane</li></ol></html>");
-    return res.end();
-  }
-
-  if ( req.url === '/create-user' ){
-    let body = [];
-    req.on('data', (chunk) => {
-      body.push(chunk);
-    })
-    req.on('end', () => {
-      const body1 = Buffer.concat(body).toString();
-      // at this point, `body` has the entire request body stored in it as a string
-      console.log(body1);
-    });
-    return res.end()
-  }
-
-}
-
-const server = http.createServer(handler);
+app.use( ( req, res, next ) =>{
+  console.log("Stuck in the middle again");
+})
+const server = http.createServer(app);
 
 server.listen('3000');
